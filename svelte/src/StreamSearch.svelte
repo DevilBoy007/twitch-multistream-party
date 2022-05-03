14effea12471b4370094d0e5cd31a1c95629b59e
Dylan Bakr <dylan_bakr@comcast.net>
2022-05-02 22:47:09 -0400

    im dumb

---
+++ svelte/src/StreamSearch.svelte
@@ -0,0 +1,63 @@
+<style>
+  .rounded
+  {
+  	max-width: max-content;
+  	padding: 1vw;
+    padding-top:0;
+  	border-radius: 12px;
+  	z-index: 1;
+  }
+</style>
+<script>
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+  let input = ''
+  let channels
+  let url
+  let params
+  async	function getGameID(game) {
+		params = new URLSearchParams({ name:game })
+		url = 'https://api.twitch.tv/helix/games?' + params
+		const response = await fetch(url,
+				 {
+					method: 'GET',
+					headers: {
+						'Authorization': 'Bearer tb0h8i9um3n6skoufnc011ay9k0vsw',
+						'Client-Id':'g4jmj1um0k0rs5b57yescjc81zv6oc'
+					}
+		})
+		.then( response => { return response.json() })
+		.catch( e => console.log(e))
+//		console.log(response.data[0].id)
+		return response.data[0].id
+	}
+  async	function getStreams(game) {
+		let gameID = await getGameID(game)
+		params = new URLSearchParams({game_id:gameID})
+		url = 'https://api.twitch.tv/helix/streams?'+params
+		const response = await fetch(url,
+			{
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer 9elniowo48r9ghy4435fgvorx1bbck',
+				'Client-Id':'g4jmj1um0k0rs5b57yescjc81zv6oc'
+			}
+		})
+		.then( response => { return response.json() })
+		.catch( e => console.log(e))
+		channels = []
+    for(let item of response.data){
+      channels = [...channels, item.user_login]
+    }
+  }
+  async function search(game){
+    await getStreams(game)
+    dispatch('search',channels)
+    channels = null // reset local list for next dispatch
+  }
+</script>
+<div class='rounded'>
+  <p>Search live game streams</p>
+  <input id='game' type='search' placeholder='Apex Legends' bind:value={input} on:keydown={(e) => e.key==='Enter'?search(input):''}/>
+  <button on:click = {search(input)}>search</button>
+</div>
